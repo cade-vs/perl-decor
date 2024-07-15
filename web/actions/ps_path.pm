1305e0b47a72edaf051947baba940a0f5c1771e3
Vladi Belperchinov-Shabanski <cade@noxrun.com>
2024-07-16 00:07:01 +0300

    maint

--- web/actions/ps_path.pm
+++ web/actions/ps_path.pm
@@ -19,6 +19,9 @@ sub main
   return "<#main_ps_path_tr_hide>" unless @$ps_path > 0;
   
   my $text;
+
+  my $menu_item_text;
+  my $menu_item_text_last;
   
   my $path_count = @$ps_path;
   for my $p ( @$ps_path )
@@ -27,11 +30,16 @@ sub main
     my $pp_id = $p->{ 'PS_ID' };
     my $icon  = $p->{ 'ICON'  };
     my $title = $p->{ 'TITLE' };
+
+    $menu_item_text .= "";
+    my $alink = de_html_alink( $reo, 'none', "$icon $title", { CLASS => 'plain', HINT => $title }, _P => $pp_id );
+    $menu_item_text .= "<p> $alink";
     
     if( $path_count <= 0 )
       {
       # current page session
-      $text .= " <b>:</b>  <img class=icon-disabled src=i/$icon> $title";
+      $text .= " <b>&raquo;</b>  <img class=icon-disabled src=i/$icon> $title";
+      $menu_item_text_last = " <b>&raquo;</b>  <img class=icon-disabled src=i/$icon> $title";
       }
     else
       {
@@ -46,6 +54,9 @@ sub main
   my $pc_str = str_countable( $pc, "[~step]", "[~steps]" );
 #  $text = "[~History]: <b>$pc</b> $pc_str $text";
   $text = "[$pc] $text";
+
+  my $menu_item_handle = html_popup_layer( $reo, VALUE => $menu_item_text, CLASS => 'popup-layer popup-layer-inline', TYPE => 'CLICK', TIMEOUT => 250, SINGLE => 1 );
+  $text = "<span class=main-menu $menu_item_handle>$menu_item_text_last</span>";
   
   # TODO: make it drop-down menu with previous steps!
 
