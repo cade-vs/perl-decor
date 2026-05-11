df7b10f276d110f2b936b9538eb144bb840be5e0
Vladi Belperchinov-Shabanski <cade@noxrun.com>
2026-05-12 01:01:57 +0300

    maint

--- bin/install-cpan-perl-modules.sh
+++ bin/install-cpan-perl-modules.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cpan CGI Plack::Request Data::Tools Exception::Sink Net::Waiter Time::Profiler Web::Reactor
-cpan Compress::Zlib Digest::SHA1 Digest::Whirlpool Clone Data::Lock Date::Format Date::Parse Date::Calc Hash::Util MIME::Base64 Scalar::Util Sys::SigAction Term::ReadKey Tie::IxHash Time::HiRes Time::HR Time::JulianDay XML::Bare Data::Structure::Util IPC::Shareable
+cpan Compress::Zlib Digest::SHA1 Digest::Whirlpool Clone Data::Lock Date::Format Date::Parse Date::Calc Hash::Util MIME::Base64 Scalar::Util Sys::SigAction Term::ReadKey Tie::IxHash Time::HiRes Time::HR Time::JulianDay XML::Bare Data::Structure::Util IPC::Shareable Crypt::PK::RSA Crypt::Argon2
 cpan File::Temp DBI DBD::Pg
 cpan Plack::Request
+cpan Term::ReadLine
 
 # ;)
 cpan CPAN
